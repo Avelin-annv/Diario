c3a6c304709435cd05df14d298b1da5d28ec6ca2
Avelin Ann <avelinannvarghese@gmail.com>
2024-04-21 23:24:36 +0530

    initial setup-2

---
+++ frontend/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./landingPage.css";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Button from "react-bootstrap/esm/Button";
+
+const LandingPage = () => {
+  return (
+    <div className="main-landing">
+      <Container>
+        <Row>
+          <div className="text-center">
+            <h1>Welcome to Diario</h1>
+            <h2>Jot down ideas!</h2>
+          </div>
+          <div className="text-center m-2">
+            <Button className="m-2">Login</Button>
+            <Button className="m-2" variant="outline-primary">
+              Sign up
+            </Button>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingPage;
