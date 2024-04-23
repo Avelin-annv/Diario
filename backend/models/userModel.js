30864bd45136ff27477459be835ca3bef6969dcb
Avelin Ann <avelinannvarghese@gmail.com>
2024-04-23 23:38:39 +0530

    model+routes

---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      unique: true,
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+userSchema.methods.matchpassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import "./bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import LandingPage from "./pages/landingPage/LandingPage";
+import MyNotesPage from "./pages/MyNotesPage";
+import Home from "./pages/Home";
+import Note from "./components/Note";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const appRouter = createBrowserRouter;
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <LandingPage /> },
+      { path: "/notes", element: <MyNotesPage /> },
+      { path: "/home", element: <Home /> },
+      { path: "/note/:id", element: <Note /> },
+    ],
+  },
+]);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
-
-reportWebVitals();
