a22d4ec8cc989819d77ae9845178731dc902fd62
Avelin Ann <avelinannvarghese@gmail.com>
2024-05-06 16:30:54 +0530

    Draw preview wrap up

---
+++ frontend/src/components/DrawList.js
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/esm/Button";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteCanvasById,
+  fetchAllCanvas,
+  fetchCanvasById,
+} from "../store/canvasSlice";
+
+const DrawList = () => {
+  const { canvases, errors, status, selectedCanvas } = useSelector(
+    (store) => store.canvas
+  );
+  const dispatch = useDispatch();
+  const getAllCanvas = async () => {
+    dispatch(fetchAllCanvas());
+  };
+  const handleViewCanvas = (id) => {
+    dispatch(fetchCanvasById(id));
+  };
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete this draw?"))
+      dispatch(deleteCanvasById(id));
+  };
+  useEffect(() => {
+    getAllCanvas();
+  }, []);
+
+  return (
+    <>
+      {canvases.length > 0 ? (
+        <>
+          {canvases?.map((canvas, index) => (
+            <Card
+              className={
+                selectedCanvas?._id === canvas?._id ? "selected-canvas" : ""
+              }
+            >
+              <Row className="accordion-header">
+                <Col lg={10}>
+                  <Card.Header>
+                    <p>{canvas.title}</p>
+                  </Card.Header>
+                </Col>
+
+                <Col>
+                  <div className=" mx-2">
+                    <Button
+                      variant="primary"
+                      className="mx-2"
+                      onClick={() => handleViewCanvas(canvas._id)}
+                    >
+                      View
+                    </Button>
+
+                    <Button
+                      variant="danger"
+                      className="mx-2"
+                      onClick={() => handleDelete(canvas._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Col>
+              </Row>
+
+              <Card.Body>
+                <blockquote className="blockquote mb-0">
+                  <footer className="blockquote-footer">
+                    Created on{" "}
+                    <span>{canvas?.createdAt?.substring(0, 10)}</span>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Card>
+          ))}
+        </>
+      ) : (
+        <h3>Your drawing list is empty! Create one</h3>
+      )}
+    </>
+  );
+};
+
+export default DrawList;
---
+++ frontend/src/components/DrawPreview.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import Konva from "konva";
+import MainContainer from "./MainContainer";
+import Button from "react-bootstrap/esm/Button";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import DrawList from "./DrawList";
+import { useDispatch, useSelector } from "react-redux";
+import { addDefaultSelectedCanvas } from "../store/canvasSlice";
+import { Link } from "react-router-dom";
+
+const DrawPreview = () => {
+  const dispatch = useDispatch();
+  const { canvases, selectedCanvas } = useSelector((store) => store.canvas);
+  useEffect(() => {
+    dispatch(addDefaultSelectedCanvas());
+  }, [canvases]);
+  useEffect(() => {
+    if (selectedCanvas) {
+      Konva.Node.create(selectedCanvas.canvasJson, "canvas-container");
+    }
+  }, [selectedCanvas]);
+  return (
+    <MainContainer title={"View your drawings"} className="bg-black">
+      <Row className="d-flex">
+        <Col>
+          <Link to="/draw/create">
+            <Button className="m-2 px-4">+ Create New!</Button>
+          </Link>
+        </Col>
+        <Col>{selectedCanvas && <h2>{selectedCanvas.title}</h2>}</Col>
+      </Row>
+      <Row>
+        <Col>
+          <DrawList />
+        </Col>
+
+        <Col lg={9}>
+          {canvases?.length > 0 && (
+            <div
+              className="canvas-wrapper canvas-preview"
+              id="canvas-container"
+            />
+          )}
+        </Col>
+      </Row>
+    </MainContainer>
+  );
+};
+
+export default DrawPreview;
